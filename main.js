7e4075860b3711a5e2affb9fa8c192e8c5fb00fd
Ana <anamartinezperdomo@gmail.com>
2025-08-15 17:43:25 +0100

    Added fonts, improved structure

--- main.js
+++ main.js
@@ -4,33 +4,33 @@ import { NeatGradient } from "https://esm.sh/@firecms/neat";
 
 // Configuración del gradient
 export const config = {
-    colors: [
-        { color: '#FF7954', enabled: true },
-        { color: '#A73C5A', enabled: true },
-        { color: '#631E50', enabled: true },
-        { color: '#E04070', enabled: true },
-        { color: '#6A1B9A', enabled: true },
-    ],
-    speed: 1.5,
-    horizontalPressure: 3,
-    verticalPressure: 4,
-    waveFrequencyX: 0,
-    waveFrequencyY: 0,
-    waveAmplitude: 0,
-    shadows: 5,
-    highlights: 0,
-    colorBrightness: 1.5,
-    colorSaturation: 2,
-    wireframe: false,
-    colorBlending: 4,
-    backgroundColor: '#FF0058',
-    backgroundAlpha: 0.25,
-    grainScale: 0,
-    grainSparsity: 0,
-    grainIntensity: 0,
-    grainSpeed: 0,
-    resolution: 0.35,
-    yOffset: 0,
+  colors: [
+    { color: "#FF7954", enabled: true },
+    { color: "#A73C5A", enabled: true },
+    { color: "#631E50", enabled: true },
+    { color: "#E04070", enabled: true },
+    { color: "#6A1B9A", enabled: true },
+  ],
+  speed: 2,
+  horizontalPressure: 3,
+  verticalPressure: 4,
+  waveFrequencyX: 0,
+  waveFrequencyY: 0,
+  waveAmplitude: 0,
+  shadows: 5,
+  highlights: 0,
+  colorBrightness: 1.5,
+  colorSaturation: 2,
+  wireframe: false,
+  colorBlending: 4,
+  backgroundColor: "#FF0058",
+  backgroundAlpha: 0.25,
+  grainScale: 0,
+  grainSparsity: 0,
+  grainIntensity: 0,
+  grainSpeed: 0,
+  resolution: 0.35,
+  yOffset: 0,
 };
 
 // Elemento HTML para el gradient
@@ -39,7 +39,7 @@ const gradientElement = document.getElementById("gradient");
 // Crear el gradient
 const neat = new NeatGradient({
   ref: gradientElement,
-  ...config
+  ...config,
 });
 
 // Limpieza si se recarga la página
